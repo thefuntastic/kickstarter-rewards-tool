d45f19034b0983a0f1e42ba9b1c8f17692ed44a0
Peter Cardwell-Gardner <peter@thefuntastic.com>
2015-03-12 14:38:49 -0500

    Cleaning up testing files for public release

--- scraper.js
+++ scraper.js
@@ -9,15 +9,45 @@ var fs = require('fs');
 //   }
 // })
 var srcs =[ 
-    "https://www.kickstarter.com/projects/lofi/four-sided-fantasy-a-game-about-the-limits-of-the",
-    "https://www.kickstarter.com/projects/2011341880/knite-and-the-ghost-lights?ref=kickspy",
-    "https://www.kickstarter.com/projects/379113968/spate",
-    "https://www.kickstarter.com/projects/kitfoxgames/moon-hunters-a-myth-weaving-rpg",
-    "https://www.kickstarter.com/projects/189665092/the-fall-dark-story-driven-exploration-in-an-alien",
-    "https://www.kickstarter.com/projects/punyhuman/blade-symphony?ref=play",
-    "https://www.kickstarter.com/projects/celsiusgs/drifter-a-space-trading-game?ref=play",
-    "https://www.kickstarter.com/projects/1499900830/sir-you-are-being-hunted?ref=play",
-    "https://www.kickstarter.com/projects/samanthazero/sentris-unleash-your-inner-musician"
+    // "https://www.kickstarter.com/projects/lofi/four-sided-fantasy-a-game-about-the-limits-of-the",
+    // "https://www.kickstarter.com/projects/2011341880/knite-and-the-ghost-lights?ref=kickspy",
+    // "https://www.kickstarter.com/projects/379113968/spate",
+    // "https://www.kickstarter.com/projects/kitfoxgames/moon-hunters-a-myth-weaving-rpg",
+    // "https://www.kickstarter.com/projects/189665092/the-fall-dark-story-driven-exploration-in-an-alien",
+    // "https://www.kickstarter.com/projects/punyhuman/blade-symphony?ref=play",
+    // "https://www.kickstarter.com/projects/celsiusgs/drifter-a-space-trading-game?ref=play",
+    // "https://www.kickstarter.com/projects/1499900830/sir-you-are-being-hunted?ref=play",
+    // "https://www.kickstarter.com/projects/samanthazero/sentris-unleash-your-inner-musician"
+    // "https://www.kickstarter.com/projects/ekuatorgames/celestian-tales-old-north-redefining-the-classic-r?ref=kickspy",
+    // "https://www.kickstarter.com/projects/coldricegames/interstellaria?ref=kickspy",
+    // "https://www.kickstarter.com/projects/1907053239/the-deer-god-a-game-of-reincarnation-pc-mac-linux?ref=kickspy",
+    // "https://www.kickstarter.com/projects/starcommand/star-command-sci-fi-meets-gamedev-story-for-ios-an?ref=kickspy",
+    // "https://www.kickstarter.com/projects/sleepninja/monsters-ate-my-birthday-cake?ref=kickspy",
+    // "https://www.kickstarter.com/projects/sfbgames/detective-grimoire-mystery-adventure-game-for-ios?ref=kickspy",
+    // "https://www.kickstarter.com/projects/soundself/soundself?ref=kickspy",
+    // "https://www.kickstarter.com/projects/musicwizard/learn-to-play-music-in-minutes-and-kickstart-your?ref=kickspy",
+    // "https://www.kickstarter.com/projects/682108903/jotun?ref=kickspy",
+    // "https://www.kickstarter.com/projects/spaniardmike/candle-a-dynamic-graphic-adventure?ref=kickspy",
+    // "https://www.kickstarter.com/projects/27467991/dashkin-a-game-of-high-speed-challenges-through-br?ref=kickspy",
+    // "https://www.kickstarter.com/projects/leafygames/pulsar-lost-colony?ref=kickspy",
+    // "https://www.kickstarter.com/projects/tammekagames/radial-g-a-high-octane-sci-fi-racer-for-the-vr-gen?ref=kickspy",
+    // "https://www.kickstarter.com/projects/1561538328/lilt-line-too?ref=kickspy",
+    // "https://www.kickstarter.com/projects/208431038/songmasters-the-music-wars?ref=kickspy",
+    // "https://www.kickstarter.com/projects/ekuatorgames/celestian-tales-old-north?ref=kickspy",
+    // "https://www.kickstarter.com/projects/14214732/colossal-kaiju-combat-expanded-and-upgraded-ouya?ref=kickspy",
+    // "https://www.kickstarter.com/projects/rupa211/rock-vibe?ref=kickspy",
+    // "https://www.kickstarter.com/projects/samread/dream-0?ref=kickspy",
+    // "https://www.kickstarter.com/projects/1400130230/oceania-online-mmo-rpg?ref=kickspy",
+    // "https://www.kickstarter.com/projects/1188957169/scale",
+    // "https://www.kickstarter.com/projects/1307515311/night-in-the-woods",
+    // "https://www.kickstarter.com/projects/infinitap/neverending-nightmares",
+    // "https://www.kickstarter.com/projects/bischoff/stasis-2d-isometric-scifi-horror-adventure-game",
+    "https://www.kickstarter.com/projects/1460250988/darkest-dungeon-by-red-hook-studios"
+
+
+
+
+
 
 ]
 
